06c6cf181d2402fabf3be3de5d96a56de5b9e7a0
Christoph Weniger <c.weniger@uva.nl>
2020-03-03 14:10:15 +0100

    Updates gp example

--- pyrofit/examples/gp.py
+++ pyrofit/examples/gp.py
@@ -18,24 +18,14 @@ class Entropy:
         return D_ij.argKmin(K, dim=1)  # (M, K) Minimum indices
 
     def __call__(self, a:Yaml, b:Yaml):
-        N = 200  # Number of sources in the model
-        kmin, kmax = 10, 60
+        N = 200  # Number of pixels 
+        kmin, kmax = 10, 50
 
         # Fixed source positions
         x = torch.linspace(-1, 1, N)
 
-        # Sample flux from normal distribution
+        # Sample flux from standard normal distribution
         y = pyro.sample("y", dist.Normal(0., 1.).expand_by((N,)))
-        #print(y.min(), y.max())
-
-        # Introduce random deviations in a
-        w1 = pyro.param("w1", torch.zeros(N))
-        da = torch.randn(1)*0.1
-        da = 0.1
-
-        # Apply
-        y = y + w1*da
-        a = a + da
 
         # Transform y
         y = y.unsqueeze(1)
@@ -43,23 +33,21 @@ class Entropy:
         # Construct vectors for distance calculation
         ind = self._kNN(y, y, kmax)
 
-        # Calculate linear distance to 4+ NN
+        # Calculate linear distance to kmin+ NNs
         d2 = (y[ind][...,:1,:] - y[ind][...,kmin:,:])**2
-        d2 = d2[:,:,0]
+        d = d2[:,:,0]**0.5
 
         # Construct weights
         w = torch.linspace(kmin, kmax-1., kmax - kmin)
-        #w = torch.ones(kmax - kmin)
         w /= w.sum()
 
         # Loss function obtained from weighted sum of log of distances
-        entropy_loss = 1.0*(-torch.log(d2**0.5)*w).sum()
+        entropy_loss = 1.0*(-torch.log(d)*w).sum()
 
         # Workaround sample call to add entropy loss to log_density
-        pyro.sample("fake", dist.Delta(torch.zeros(1), log_density = -entropy_loss), obs = torch.zeros(1))
+        #pyro.sample("fake", dist.Delta(torch.zeros(1), log_density = -entropy_loss), obs = torch.zeros(1))
 
         # Construct spectrum
-        mu = (y*a*torch.exp(-(x.unsqueeze(0) -
-            x.unsqueeze(1))**2/b**2/2)).sum(0)
-        pyro.sample("obs", dist.Normal(mu, 1.0))
+        mu = (y*(a/b)*torch.exp(-(x.unsqueeze(0) - x.unsqueeze(1))**2/b**2/2)).sum(0)
+        pyro.sample("obs", dist.Normal(mu, 0.1))
         observe("mu", mu)
