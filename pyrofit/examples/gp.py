88458b09f4d58c440dd54a0bf72233209f441d7d
Christoph Weniger <cweniger@node001.cm.cluster>
2020-03-20 12:25:33 +0100

    gp.py

--- pyrofit/examples/gp.py
+++ pyrofit/examples/gp.py
@@ -9,6 +9,7 @@ import random
 class Entropy:
     def __init__(self, device = 'cpu'):
         self.device = device
+        self.pref = 0.00001
 
     @staticmethod
     def _kNN(x, y, K):
@@ -18,9 +19,9 @@ class Entropy:
         D_ij = ((x_i - y_j) ** 2).sum(-1)  # (M, N) symbolic matrix of squared distances
         return D_ij.argKmin(K, dim=1)  # (M, K) Minimum indices
 
-    def __call__(self, a:Yaml, b:Yaml, use_entropy: Yaml=True):
+    def __call__(self, a:Yaml, b:Yaml, c:Yaml, use_entropy: Yaml=True):
         N = 200  # Number of pixels 
-        kmin, kmax = 1, 2
+        kmin, kmax = 1, 5
 
         # Fixed source positions
         x = torch.linspace(-1+1.0/N, 1-1.0/N, N, device = self.device)
@@ -28,6 +29,11 @@ class Entropy:
         # Sample flux from standard normal distribution
         sigma = torch.tensor(1., device = self.device)
         y = pyro.sample("y", dist.Normal(0., sigma).expand_by((N,)))
+
+        use_entropy = True
+
+        if self.pref < 1.:
+            self.pref *= 1.003
 
         if use_entropy:
             # Transform y
@@ -48,8 +54,8 @@ class Entropy:
             w /= w.sum()
 
             # Loss function obtained from weighted sum of log of distances
-            entropy_loss = 2.0*(-torch.log(d)*w).sum() #+ 2*torch.log(a)
-            #print(entropy_loss)
+            entropy_loss = self.pref*2.0*(-torch.log(d)*w).sum() #+ 2*torch.log(a)
+            print(self.pref, entropy_loss)
 
             # Workaround sample call to add entropy loss to log_density
             pyro.sample("fake", dist.Delta(torch.zeros(1, device = self.device), log_density = -entropy_loss), obs = torch.zeros(1, device = self.device))
@@ -57,7 +63,7 @@ class Entropy:
         # Construct spectrum
         y = y.unsqueeze(1)
         mu = (y*(a/b)*torch.exp(-(x.unsqueeze(0) - x.unsqueeze(1))**2/b**2/2)).sum(0)
-        #mu = mu + y[:,0]*c
+        mu = mu + y[:,0]*c
         pyro.sample("obs", dist.Normal(mu, 0.1))
         observe("mu", mu)
         #print(a, b, c)
